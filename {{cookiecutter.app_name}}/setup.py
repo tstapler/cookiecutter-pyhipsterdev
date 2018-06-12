8b91ad7268f897b23a1a8452a0bd1c8fd22777b5
Tyler Stapler <tyler.stapler@workiva.com>
2018-06-12 09:33:09 -0500

    Migrate changes from other projects

--- {{cookiecutter.app_name}}/setup.py
+++ {{cookiecutter.app_name}}/setup.py
@@ -9,15 +9,19 @@ __version__ = '{{cookiecutter.version}}'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
-install_requires = [re.sub(r"--hash=.*", "",x.strip()) for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [
+    re.sub(r"--hash=.*", "", x.strip()) for x in all_reqs if 'git+' not in x
+]
+dependency_links = [
+    x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')
+]
 
 setup(
     name='{{cookiecutter.app_name}}',
